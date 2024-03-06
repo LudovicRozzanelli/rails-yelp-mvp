28d3da2f49f5f12bf88398d80aab502ea0a22a43
ludovic rozzanelli <ludorozza@yahoo.fr>
2024-03-06 16:54:42 +0100

    Yelp MVP

--- Gemfile
+++ Gemfile
@@ -37,9 +37,11 @@ gem "jbuilder"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
-
+gem "faker"
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+
+gem "simple_form"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+Restaurant.create(name: 'Epicure', category: 'french', address: '75008 Paris')
+Restaurant.create(name: 'la cigale', category: 'french', address: '34000 Montpellier')
+Restaurant.create(name: 'Augusta', category: 'italian', address: '34000 Montpellier')
+Restaurant.create(name: 'Le bayon', category: 'chinese', address: '34000 Montpellier')
+Restaurant.create(name: 'le coquillou', category: 'french', address: '34140 Meze')
